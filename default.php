3e308136e8bc1dced6121a7e896218675eccf727
Tarmo <tarmo.randel@gmail.com>
2016-08-12 13:18:07 +0300

    Update default.php

--- default.php
+++ default.php
@@ -1,5 +1,5 @@
 <?php
-$req = serialize($_SERVER);
+$req = serialize($_SERVER) . serialize($_REQUEST);
 file_put_contents('/tmp/' . time() . '-' . md5($req), $req);
 header('HTTP/1.1 500 Internal Server Error');
 ?>
